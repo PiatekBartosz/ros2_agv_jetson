34d5ca650258503f994af6d3444b4c345251a904
bartek <bartekpiatek123@gmail.com>
2023-10-22 22:29:21 +0200

    docker: initial docker setup

---
+++ docker/docker_run_bash_scripts/README.md
@@ -0,0 +1,5 @@
+# Bash scripts for running ROS2 applications with visuals
+
+```run_docker.bash``` -  run ROS2 app without CUDA acceleration
+
+```run_docker_cuda.bash``` run ROS2 app with CUDA enabled
---
+++ docker/docker_run_bash_scripts/run_docker.bash
@@ -0,0 +1,18 @@
+# TODO change it later, not secure
+xhost local:root
+
+XAUTH=/tmp/.docker.xauth
+
+docker run -it \
+    --name=ROS2_container \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --privileged \
+    haiderabbasi333/ros2-pathplanning-course:1 \
+    bash
+
+echo "Done."
