44f6ec153af42c7f9a0a0383e0576272ec44e0ea
bartek <bartekpiatek123@gmail.com>
2023-11-26 01:21:40 +0100

    docker: first working version & cleanup

--- old/new try cuda desktop/build.bash
+++
@@ -1,8 +0,0 @@
-docker buildx create --use  --driver-opt network=host --name MultiPlatform
-
-# build the image for two different platforms and push the images
-docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  -f Dockerfile \
-  -t output \
-  --push .
